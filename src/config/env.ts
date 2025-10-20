89d099c0c4e2df4b3d1a294036ca5108cf32c171
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-10-21 01:06:39 +0600

    feat: add Cloudinary environment variables to configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,10 +7,22 @@ interface EnvVariables {
   NODE_ENV: "development" | "production";
   PORT: string;
   DATABASE_URL: string;
+  CLOUDINARY: {
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+  };
 }
 
 const loadEnvVariable = (): EnvVariables => {
-  const requiredEnvVariables = ["NODE_ENV", "PORT", "DATABASE_URL"];
+  const requiredEnvVariables = [
+    "NODE_ENV",
+    "PORT",
+    "DATABASE_URL",
+    "CLOUDINARY_CLOUD_NAME",
+    "CLOUDINARY_API_KEY",
+    "CLOUDINARY_API_SECRET",
+  ];
 
   requiredEnvVariables.forEach((key) => {
     if (!process.env[key]) {
@@ -22,6 +34,11 @@ const loadEnvVariable = (): EnvVariables => {
     NODE_ENV: process.env.NODE_ENV as "development" | "production",
     PORT: process.env.PORT as string,
     DATABASE_URL: process.env.DATABASE_URL as string,
+    CLOUDINARY: {
+      CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+      CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+      CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+    },
   };
 };
 
